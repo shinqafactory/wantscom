00db9b2f232fc302acfe085abde83195e0f67a82
Taichi <j2002t.o@gmail.com>
2013-02-17 11:04:57 +0900

    answer model spec made by taichi

--- spec/models/ans_spec.rb
+++ spec/models/ans_spec.rb
@@ -1,5 +1,22 @@
+# encoding: utf-8
 require 'spec_helper'
 
 describe Ans do
-  pending "add some examples to (or delete) #{__FILE__}"
+    
+  before(:each) do
+    @attr = {:ans_content => "Answer Sample",
+               :ans_title => "Answer Title",
+               :ans_url => "Answer Url",
+               :ans_date => "Answer Date",}
+    @que1 = Factory(:questions, :answer => @answer, :created_at => 1.day.ago)
+    @que2 = Factory(:questions, :answer => @answer, :created_at => 30.day.ago)
+  end
+
+  it "Question is attributed to Answer" do
+    @attr.should respond_to(:questions)
+  end
+
+  it "should have the right questions in the right order" do
+    @attr.questions.should == [@que2, @que1]
+  end
 end
