9a3e56a21a8b90e2df5e9f3deb0b823f282bd64f
Kazuhiro Nishiyama <248mak@gmail.com>
2013-02-17 11:33:03 +0900

    I changed a few spec

--- spec/models/ans_spec.rb
+++ spec/models/ans_spec.rb
@@ -19,4 +19,38 @@ describe Ans do
   it "should have the right questions in the right order" do
     @attr.questions.should == [@que2, @que1]
   end
+  　　＃ユーザーID付与に関するチェック
+  describe "validations" do
+    it "should require a user id" do
+      Ans.new(@attr).should_not be_vaild
+    end
+    ＃内容に関するバリデーション
+    ＃既定以上の長い内容
+    it "should reject nonblank content" do
+      no_ans_content = ans.new(@attr.merge(:ans_content => ""))
+      no_ans_content.should_not be_vaild
+    end
+    it "should reject long content" do
+       long_ans_content = ans.new(@attr.merge(:ans_content => "a"*751 ))
+       long_ans_content.should_not be_vaild
+    end
+    ＃タイトルに関するバリデーション
+    it "should reject nonblank　title " do
+         no_ans_titie = ans.new(@attr.merge(:ans_title => ""))
+         no_ans_title.should_not be_vaild
+    end
+    it "should reject long title" do
+       long_ans_title = ans.new(@attr.merge(:ans_title => "a"*151 ))
+       long_ans_title.should_not be_vaild
+    end
+    ＃URLに関するバリデーション
+    it "should reject nonblank URL" do
+       no_ans_url = ans.new(@attr.merge(:ans_url => ""))
+       no_ans_url.should_not be_vaild
+    end
+    it "should reject long URL" do
+       long_ans_url = ans.new(@attr.merge(:ans_url => "a"*501 ))
+       long_ans_url.should_not be_vaild
+    end
+  end 
 end
