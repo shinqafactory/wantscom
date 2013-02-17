b645fe263518a5e72873c2c4bc6710278f895af4
Kazuhiro Nishiyama <248mak@gmail.com>
2013-02-17 11:39:29 +0900

    Modified 20130217

--- spec/models/ans_spec.rb
+++ spec/models/ans_spec.rb
@@ -19,13 +19,13 @@ describe Ans do
   it "should have the right questions in the right order" do
     @attr.questions.should == [@que2, @que1]
   end
-  　　＃ユーザーID付与に関するチェック
+  　　#ユーザーID付与に関するチェック
   describe "validations" do
     it "should require a user id" do
       Ans.new(@attr).should_not be_vaild
     end
-    ＃内容に関するバリデーション
-    ＃既定以上の長い内容
+    #内容に関するバリデーション
+    #既定以上の長い内容
     it "should reject nonblank content" do
       no_ans_content = ans.new(@attr.merge(:ans_content => ""))
       no_ans_content.should_not be_vaild
@@ -34,7 +34,7 @@ describe Ans do
        long_ans_content = ans.new(@attr.merge(:ans_content => "a"*751 ))
        long_ans_content.should_not be_vaild
     end
-    ＃タイトルに関するバリデーション
+    #タイトルに関するバリデーション
     it "should reject nonblank　title " do
          no_ans_titie = ans.new(@attr.merge(:ans_title => ""))
          no_ans_title.should_not be_vaild
@@ -43,7 +43,7 @@ describe Ans do
        long_ans_title = ans.new(@attr.merge(:ans_title => "a"*151 ))
        long_ans_title.should_not be_vaild
     end
-    ＃URLに関するバリデーション
+    #URLに関するバリデーション
     it "should reject nonblank URL" do
        no_ans_url = ans.new(@attr.merge(:ans_url => ""))
        no_ans_url.should_not be_vaild
