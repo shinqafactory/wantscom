26d7e8d899aeae9e33d4c7102efa9d79d22033f1
Kazuhiro Nishiyama <248mak@gmail.com>
2013-02-24 16:24:54 +0900

    kazu

--- spec/models/ans_spec.rb
+++ spec/models/ans_spec.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 #【テスト未了事項】
 #(1)他モデルからの接続関係のテスト
 #（２）URLに関するバリデーションの拡充チェック
@@ -12,65 +14,41 @@
 #(4)回答URLに関するバリデーション
 #次の場合はエラーとする（501文字以上、空欄、内容に以下が含まれないもの（http,https))
 
-# encoding: utf-8
 require 'spec_helper'
 
 describe Ans do
     
   before(:each) do
-    @attr = {:ans_content => "Answer Sample",
-               :ans_title => "Answer Title",
-               :ans_url => "Answer url",
-               :ans_date => "Answer Date",}
-    @que1 = Factory(:questions, :answer => @answer, :created_at => 1.day.ago)
-    @que2 = Factory(:questions, :answer => @answer, :created_at => 30.day.ago)
+  @attr = [:ans_content => "Answer Sample",
+             :ans_title => "Answer Title",
+             :ans_url   => "Answer url"
+             ]
   end
-
-  it "質問は回答に帰属する。" do
-    @attr.should respond_to(:questions)
-  end
-
-  it "回答は正しい順番で並ぶ。" do
-    @attr.questions.should == [@que2, @que1]
-  end
-
-  describe "質問関連のバリデーション" do
-    it "ユーザーIDは必要とされる。" do
-      Ans.new(@attr).should_not be_vaild
-    end
-
+  
+  describe "バリデーション" do
     it "空欄の回答内容はエラーとなる。" do
-      no_ans_content = ans.new(@attr.merge(:ans_content => ""))
+      no_ans_content = Ans.new(@attr.merge(:ans_content => ""))
       no_ans_content.should_not be_vaild
     end
     it "長い（751文字以上）の回答内容はエラーとなる。" do
-       long_ans_content = ans.new(@attr.merge(:ans_content => "a"*751 ))
+       long_ans_content = Ans.new(@attr.merge(:ans_content => "a"*751 ))
        long_ans_content.should_not be_vaild
     end
     it "空欄の回答題名はエラーとなる。" do
-         no_ans_titie = ans.new(@attr.merge(:ans_title => ""))
-         no_ans_title.should_not be_vaild
+       no_ans_titie = Ans.new(@attr.merge(:ans_title => ""))
+       no_ans_title.should_not be_vaild
     end
     it "長い（151文字以上）の回答題名はエラーとなる。" do
-       long_ans_title = ans.new(@attr.merge(:ans_title => "a"*151 ))
+       long_ans_title = Ans.new(@attr.merge(:ans_title => "a"*151 ))
        long_ans_title.should_not be_vaild
     end
     it "空欄の回答URLはエラーとなる。" do
-       no_ans_url = ans.new(@attr.merge(:ans_url => ""))
+       no_ans_url = Ans.new(@attr.merge(:ans_url => ""))
        no_ans_url.should_not be_vaild
     end
     it "長い（500文字以上）の回答URLはエラーとなる。" do
-       long_ans_url = ans.new(@attr.merge(:ans_url => "a"*501 ))
+       long_ans_url = Ans.new(@attr.merge(:ans_url => "a"*501 ))
        long_ans_url.should_not be_vaild
     end
-    it "回答URLはhttpを含んっでいる。" do
-           http_ans_url = ans.new(@attr.merge(:ans_url => "http" ))
-           http_ans_url.should be_vaild
-    end
-    it "回答URLはhttpsを含んっでいる。" do
-           https_ans_url = ans.new(@attr.merge(:ans_url => "https" ))
-           https_ans_url.should be_vaild
-    end
-
-  end 
+  end
 end
