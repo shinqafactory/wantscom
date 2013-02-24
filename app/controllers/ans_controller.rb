86dae345adbbf41704e6234e113b5e5d69853ed9
Kazuhiro Nishiyama <248mak@gmail.com>
2013-02-24 15:01:16 +0900

    nishiyama

--- app/controllers/ans_controller.rb
+++ app/controllers/ans_controller.rb
@@ -1,2 +1,21 @@
 class AnsController < ApplicationController
+  def create
+   @ans = ans.new(params[:que])
+        if @ans.save
+          redirect_to @que
+        else
+          @title = "Sign up"
+          render 'que'
+    end
+  end
+  def destroy
+    @ans = ans.new(params[:que])
+        if @ans.save
+          sign_in @que
+        redirect_to @que
+        else
+          @title = "Sign up"
+          render 'que'
+        end
+  end
 end
