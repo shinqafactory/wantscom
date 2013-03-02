a7001fbf1cf31bf5a7b5daff18e90357e9f55979
築城賢司 <tsuiki@shinqafactory.com>
2013-03-02 17:32:05 +0900

    update

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,24 @@
+class QuestionsController < ApplicationController
+  
+  # 質問の新規登録
+  def create
+    @question = Question.new(params[:que_id])
+    if @question.save
+      redirect_to @question, notice: "質問を登録しました。"
+    else
+      render "home"
+    end
+  end
+
+  # 質問の削除
+  def destroy
+    @question = Question.find(params[:que_id])
+    @question.assign_attributes(params[:que_id])
+    if @question.save
+      redirect_to @question, notice: "質問を削除しました。"
+    else
+      render "home"
+    end
+  end
+  
+end
