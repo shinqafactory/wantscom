06f2ab5a3a002fb3cd75b23ef346dc1745fb12f1
Kazuhiro Nishiyama <248mak@gmail.com>
2013-03-03 16:28:38 +0900

    I modified a few change. In detail,add about models action and language and  controller new

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,18 +1,24 @@
 # encoding: utf-8
 
-#【テスト未了事項】
-#(1)他モデルからの接続関係のテスト
-#(2)回答URLに関するバリデーション
-#次の場合はエラーとする（501文字以上、空欄、内容に以下が含まれないもの（http,https))
+#####################残作業（未実装）###########################
+#・他モデルからの接続関係のテスト
+#　理由：作業方法が不明確であるため
+#・回答URLに関する長さ・実在性を除いたテスト（形式フォーマット等）
+# 理由：invaildとすべき状況について把握不十分のため
+#　例示　次の場合はエラーとする（501文字以上、空欄、内容に以下が含まれないもの（http,https))
+############################################################
 
 #【テスト内容】
-#(1)ユーザーID付与に関するチェック
-#ユーザーIDがない場合はエラーとする。
-#(2)回答内容（answer_content)に関するバリデーション
-#次の場合はエラーとする（751文字以上、空欄）
-#(3)回答タイトル（ans_title)に関するバリデーション
-#次の場合はエラーとする（151文字以上、空欄）
-
+#1.回答事項（answer_content)に関するバリデーション
+# 1-1.値が無い場合はエラーとする
+# 1-2.桁数が750文字を超える場合はエラーとする
+#2.回答タイトル（answer_title)に関するバリデーション
+#　2-1.値が無い場合はエラーとする
+# 2-2.桁数が150文字を超える場合はエラーとする
+#3.回答URL（answer_url)に関するバリデーション
+#　3-1.値が無い場合はエラーとする
+# 3-2.桁数が500文字を超える場合はエラーとする
+# 3-3.(未実装参照）
 
 require 'spec_helper'
 
@@ -24,31 +30,44 @@ before(:each) do
              :answer_url   => "Answer url"
   }
 end  
-describe "バリデーション" do
-
-    it "(1-1)空欄の回答内容はエラーとなる。" do
+describe "1.回答事項（answer_content)バリデーションチェック" do
+    it "1-1.回答事項(answer_content)が空白の時：エラーが発生すること" do
       no_content = Answer.new(@attr.merge(:answer_content => ""))
       no_content.should_not be_valid
     end
-    it "(1-2)長い（751文字以上）の回答内容はエラーとなる。" do
-       long_answer_content = Answer.new(@attr.merge(:answer_content => "a"*751 ))
-       long_answer_content.should_not be_valid
+    it "1-2.回答事項(answer_content)の文字数が750文字を超える時：エラーが発生すること" do
+      long_answer_content = Answer.new(@attr.merge(:answer_content => "a"*751 ))
+      long_answer_content.should_not be_valid
     end
-    it "(2-1)空欄の回答題名はエラーとなる。" do
-       no_title = Answer.new(@attr.merge(:answer_title => ""))
-       no_title.should_not be_valid
+  describe "2.回答タイトル（answer_title)バリデーションチェック" do    
+    it "2-1.回答タイトル(answer_title)が空白の時：エラーが発生すること" do
+      no_title = Answer.new(@attr.merge(:answer_title => ""))
+      no_title.should_not be_valid
     end
-    it "(2-2)長い（151文字以上）の回答題名はエラーとなる。" do
-       long_answer_title = Answer.new(@attr.merge(:answer_title => "a"*151 ))
-       long_answer_title.should_not be_valid
+    it "2-2.回答タイトル(answer_title)の文字数が150文字を超える時：エラーが発生すること"do
+      long_answer_title = Answer.new(@attr.merge(:answer_title => "a"*151 ))
+      long_answer_title.should_not be_valid
     end
-    it "(3-1)空欄の回答URLはエラーとなる。" do
-       no_answer_url = Answer.new(@attr.merge(:answer_url => ""))
-       no_answer_url.should_not be_valid
+  describe "3.回答URL（answer_url)バリデーションチェック" do    
+    it "3-1.回答URL(answer_url)が空白の時：エラーが発生すること" do
+     no_answer_url = Answer.new(@attr.merge(:answer_url => ""))
+     no_answer_url.should_not be_valid
     end
-    it "(3-2)長い（500文字以上）の回答URLはエラーとなる。" do
-       long_answer_url = Answer.new(@attr.merge(:answer_url => "a"*501 ))
-       long_answer_url.should_not be_valid
+    it "3-2.回答タイトル(answer_url)の文字数が501文字を超える時：エラーが発生すること" do
+     long_answer_url = Answer.new(@attr.merge(:answer_url => "a"*501 ))
+     long_answer_url.should_not be_valid
+    end
+    it "変更未了" do
+     invaild_answer_url = Answer.new(@attr.merge(:answer_url => *** ))
+      　　invaild_answer_url.should_not be_valid
+    end
+ 　　　 it "変更未了" do
+     invaild_answer_url = Answer.new(@attr.merge(:answer_url => *** ))
+     invaild_answer_url.should_not be_valid
+    end
+  　　　 it "変更未了" do
+      　   invaild_answer_url = Answer.new(@attr.merge(:answer_url => *** ))
+     invaild_answer_url.should_not be_valid
     end
   end
 end
