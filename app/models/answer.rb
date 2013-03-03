9c7dc17a1fd0241722f34d209db608880e30cfee
Taichi <j2002t.o@gmail.com>
2013-03-03 17:36:18 +0900

    modified

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,4 @@
+﻿# encoding: utf-8
 class Answer < ActiveRecord::Base
     attr_accessible :answer_content, :answer_date, :answer_ent_kbn, :answer_id, :answer_id_bn, :answer_pic, :answer_question_id, :answer_title, :answer_url, :answer_use_id, :question_id_bn
     belongs_to :question
@@ -9,10 +10,10 @@ class Answer < ActiveRecord::Base
     validates  :answer_title, presence: true, 
     length: { minimum: 1, maximum: 150 , allow_blank: true }
     #回答url（answer_url）の入力チェック
-    validates  :answer_url, presence: true, 
+    validates  :answer_url, presence: true,
     length: { minimum: 1, maximum: 500 , allow_blank: true }
-    format: { with ***** }  
+    #format: { with ***** }  
     #回答入力区分（answer_ent_kbn）の入力チェック
-    validates  :answer_ent_kbn, presence: true, 
-    format: { with ***** }
+    validates  :answer_ent_kbn, presence: true
+    #format: { with ***** }
   end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,3 +1,4 @@
+﻿# encoding: utf-8
 require 'spec_helper'
 
 describe AnswersController do
