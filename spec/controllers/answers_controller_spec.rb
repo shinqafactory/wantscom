e8cba62892ea92b6e299247c36dc297ecd3f875a
Kazuhiro Nishiyama <248mak@gmail.com>
2013-03-10 16:35:10 +0900

    modified

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,13 +1,14 @@
 ﻿# encoding: utf-8
 require 'spec_helper'
+ describe AnswersController do
 
-@attr = {:answer_content => "Answer Sample",
+  @attr = {:answer_content => "Answer Sample",
+           :answer_id => "1",
            :answer_title => "Answer Title",
            :answer_url   => "Answer url",
            :answer_ent_kbn => "1"
 }
 
-describe AnswersController do
     #render_views
     #describe "1.アクセスコントロール" do
      # it "1-1.権限のないもののcreateへのアクセス：エラーを返す" do
@@ -19,29 +20,31 @@ describe AnswersController do
        # response.should redirect_to(root_path)
      # end
     #end
-    describe "2.createに関するテスト" do
+   describe "2.createに関するテスト" do
       #before(:each) do
         #@user = test_sign_in(Factory(:user))
       #end
-      describe "2-1.回答失敗時" do
-         before(:each) do
-           @attr = { :answer_content => "" }
-         end
-         it "2-1-1.回答内容がない投稿はエラーである。" do
-           lambda do
-         end
-         it "2-1-2.回答内容がない投稿は投稿ページへ遷移する。" do
-           post :create, :answer => @attr
-           response.should render_template()
-         end
+     describe "2-1.回答失敗時" do
+       before(:each) do
+         @attr = { :answer_content => "" }
        end
-       describe "2-2.回答投稿成功時" do
-         before(:each) do
-           @attr = { :answer_content => "Lorem ipsum" }
-         end
-         it "2-2-1.回答は投稿される。" do
-           lambda do
-             post :create, :answer => @attr
+       it "2-1-1.回答内容がない投稿はエラーである。" do
+         lambda do
+           post :create, :answer_id => @attr
+         end.should_not change(answer, :count)
+       end
+       it "2-1-2.回答内容がない投稿は投稿ページへ遷移する。" do
+         post :create, :answer => @attr
+         response.should render_template()
+       end
+     end
+     describe "2-2.回答投稿成功時" do
+       before(:each) do
+         @attr = { :answer_content => "Lorem ipsum" }
+       end
+       it "2-2-1.回答は投稿される。" do
+         lambda do
+           post :create, :answer => @attr
            end.should change(answer, :count).by(1)
          end
          it "2-2-2.回答の投稿後は投稿ページへ遷移する。" do
@@ -54,29 +57,30 @@ describe AnswersController do
          end
        end
      end
-     describe "destroyに関するテスト" do
-       #describe "ユーザ権限外のないもの" do
-        # before(:each) do
-           #@user = Factory(:user)
-           #wrong_user = Factory(:user, :email => Factory.next(:email))
-           #test_sign_in(wrong_user)
-           #@micropost = Factory(:micropost, :user => @user)
-         #end
-         #it "権限がないためアクセスが否定される" do
-           #delete :destroy, :id => @answer
-           #response.should redirect_to(root_path)
-         #end
-       #end   
-       describe "ユーザ権限のあるもの" do
-         #before(:each) do
-          # @user = test_sign_in(Factory(:user))
-          # @micropost = Factory(:micropost, :user => @user)
-         #end
-         it "回答は削除される。" do
-           lambda do 
-             delete :destroy, :id => @micropost
-           end.should change(answer, :count).by(-1)
-         end
+   end
+   describe "destroyに関するテスト" do
+     #describe "ユーザ権限外のないもの" do
+       #before(:each) do
+         #@user = Factory(:user)
+         #wrong_user = Factory(:user, :email => Factory.next(:email))
+         #test_sign_in(wrong_user)
+         #@micropost = Factory(:micropost, :user => @user)
+       #end
+       #it "権限がないためアクセスが否定される" do
+         #delete :destroy, :id => @answer
+         #response.should redirect_to(root_path)
+       #end
+     #end   
+     describe "ユーザ権限のあるもの" do
+       #before(:each) do
+         # @user = test_sign_in(Factory(:user))
+         # @micropost = Factory(:micropost, :user => @user)
+       #end
+       it "回答は削除される。" do
+         lambda do 
+           delete :destroy, :id => @micropost
+         end.should change(answer, :count).by(-1)
        end
      end
    end
+   
