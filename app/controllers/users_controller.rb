a686cca7a7934710376056dbe84c2112f2737b84
taichio <t.otsuka@shinqafactory.com>
2013-04-07 13:00:55 +0900

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 # encoding: utf-8
 class UsersController < ApplicationController
-#  before_filter :authenticate_user! 
-#  before_filter :get_user
+  before_filter :authenticate_user! 
+  before_filter :get_user
   
   # ユーザプロフィール表示処理
   def show
     # ユーザIDに紐づいたquestionとanswerを５件取得
  #   @question = @user.questions.find(:all, :limit => 5)
  #   @answer = @user.answers.find(:all, :limit => 5)
-     @question = Question.find(:all)
-     @answer = Answer.find(:all)
+     @question = Question.find(params[:id])
+     @answer_new = Answer.new
+     @answer_all = Answer.find_all_by_answer_ent_kbn_and_answer_question_id('1',@question.id)
+
   end
 
   def questions
