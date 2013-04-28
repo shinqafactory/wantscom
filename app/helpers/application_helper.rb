8709d647babd568f457370c65d497ecea48af9d5
築城賢司 <tsuiki@shinqafactory.com>
2013-04-28 10:25:55 +0900

    hepler add

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# encoding: utf-8
+
 module ApplicationHelper
+  
+  #ユーザーIDを元にユーザー名を取得
+  def get_user_name(user_id)
+    @user_name = User.select("username").find_by_id(user_id)
+    return @user_name.username
+  end
+  
 end
