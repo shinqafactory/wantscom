6b8489d633cbbe28723d803bfda8678bff474a38
築城賢司 <tsuiki@shinqafactory.com>
2013-05-02 16:57:14 +0900

    layout modified

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -6,18 +6,20 @@ class TopController < ApplicationController
     @question_new = Question.new
     @question_all = Question.find_all_by_que_ent_kbn('1')    
   end
+  
   def company
      @users = current_user
   end
+  
   def contact
      @users = current_user
   end
+  
   def privacy_policy
      @users = current_user
   end
+  
   def terms
      @users = current_user
   end
-  
-  
 end
